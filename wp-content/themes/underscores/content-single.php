b461664b1f04a9bc1a66897f5ed6020579036c63
sarvesh <sarvesh@sjinnovation.com>
2014-11-02 20:05:15 +0530

    style and gallery changes for catering and destinations

--- wp-content/themes/underscores/content-single.php
+++ wp-content/themes/underscores/content-single.php
@@ -4,7 +4,7 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('no-margin'); ?>>
 	<?php 
 		remove_filter( 'the_content', 'wpautop' ); 
 		the_content(); ?>
