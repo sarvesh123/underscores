8abfcaff621d308c53daf4c8df6cfd9aefd8f1d7
sarvesh <sarvesh@sjinnovation.com>
2014-11-02 18:47:40 +0530

    trim single page

--- wp-content/themes/underscores/footer.php
+++ wp-content/themes/underscores/footer.php
@@ -9,14 +9,6 @@
 ?>
 
 	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'underscores' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'underscores' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'underscores' ), 'underscores', '<a href="http://underscores.me/" rel="designer">Sarvesh</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
